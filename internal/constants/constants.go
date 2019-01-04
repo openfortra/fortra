6bfb6cf4f08a3c06817213534faf477e198008f7
Gabe <redhatrises@gmail.com>
2019-01-04 15:23:17 -0700

    Add contants package

---
+++ internal/constants/constants.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+	DefaultSchemaType     = "openfortra"
+	DefaultSchemaVersion  = "1.0"
+	DefaultMode           = "local"
+	DefaultUserDirLinux   = ".fortra"
+	DefaultUserConfigFile = "config.yaml"
+)
